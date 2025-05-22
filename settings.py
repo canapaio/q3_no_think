0f77af65f011fc4899d7ae0d648b5ca21937ebda
Canapaio <canapaio@ewasd.eu>
2025-05-23 01:04:44 +0200

    Update settings.py 002

--- settings.py
+++ settings.py
@@ -2,15 +2,10 @@ from cat.mad_hatter.decorators import plugin
 from pydantic import BaseModel, Field, field_validator
 
 
-class Think(Enum):
-    on = "No Think on"
-    off = "No Think off"
-
-class RemoveThink(Enum):
-    on = "Remove <Think> on"
-    off = "Remove <Think> off"
-
-
 class MySettings(BaseModel):
-    Think: Think = Think.on
-    RemoveThink: RemoveThink = RemoveThink.on
+    No_Think: bool = True
+    Remove_Think: bool = True
+
+@plugin
+def settings_model():
+    return MySettings
