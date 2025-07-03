72b86d183de60387dc4aa8141a16b95774618df9
Canapaio <canapaio@ewasd.eu>
2025-07-03 07:54:46 +0200

    Update q3nt-app.py

--- q3nt-app.py
+++ q3nt-app.py
@@ -21,7 +21,6 @@ def before_cat_stores_episodic_memory(doc, cat):
 def before_cat_sends_message(message, cat):
     settings = cat.mad_hatter.get_plugin().load_settings()
     if settings['Remove_Think']:
-        settings = cat.mad_hatter.get_plugin().load_settings()
         message["content"] = re.sub(r'<think>.*?</think>', '', message["content"], flags=re.DOTALL | re.IGNORECASE)
     return message
 
@@ -31,4 +30,4 @@ def cat_recall_query(user_message, cat):
     if settings['No_Think']:
         if user_message.startswith('/no_think'):
             user_message = user_message[len('/no_think'):]    
-    return new_query
+    return user_message
